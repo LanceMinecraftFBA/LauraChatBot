d1f259a4758f885935da1f053a4d98329cff78c8
TheLance <98957643+LanceMinecraftFBA@users.noreply.github.com>
2023-06-14 21:52:46 +0300

    Create DrinksProfile.cs

---
+++ src/Types/Games/DrinksProfile.cs
@@ -0,0 +1,9 @@
+using LauraChatManager.Types.Games.Enum;
+
+namespace LauraChatManager.Types.Games {
+    public class DrinksProfile {
+        public DrinkType Drink;
+        public double Liters;
+        public DateTime? NextDrinking;
+    }
+}
