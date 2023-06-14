b521345b2a447b8d757e1cc040b4ef2879dd65f2
TheLance <98957643+LanceMinecraftFBA@users.noreply.github.com>
2023-06-14 22:02:06 +0300

    Create ChatHandler.cs

---
+++ src/Handles/UpdateHandler/Message/ChatHandler.cs
@@ -0,0 +1,9 @@
+using Telegram.Bot;
+
+namespace LauraChatManager.Handles.UpdateHandlers.Message {
+    public class ChatHandler {
+        public static async Task Invoke(ITelegramBotClient bot, Telegram.Bot.Types.Message msg) {
+            
+        }
+    }
+}
