5dc7c61cb2dce42bfa7792c609ff31fbc5df927b
TheLance <98957643+LanceMinecraftFBA@users.noreply.github.com>
2023-06-14 21:50:41 +0300

    Create Admin.cs

---
+++ src/Types/Admin.cs
@@ -0,0 +1,9 @@
+using LauraChatManager.Types.Enums;
+
+namespace LauraChatManager.Types {
+    public class Admin {
+        public long Id;
+        public string Name;
+        public AdminType Priveleges;
+    }
+}
