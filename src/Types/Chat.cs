1b2c89401da0064bd1bd749278b8bdb8d37be5a2
TheLance <98957643+LanceMinecraftFBA@users.noreply.github.com>
2023-06-14 21:50:09 +0300

    Create Chat.cs

---
+++ src/Types/Chat.cs
@@ -0,0 +1,44 @@
+using LauraChatManager.Types.Enums;
+
+namespace LauraChatManager.Types {
+    public class Chat {
+        public long Id;
+        public int MaxWarns;
+        public int Antispam;
+        public bool AsActive;
+        public bool DetectUrl;
+        public long ReportStorage;
+        public CaptchaType CaptchaButton;
+        public bool NoBadWordsActive;
+        public string? Rules;
+        public string? CustomHello;
+        public string? ChSticker;
+        public PunishType Punish;
+        public int MaxCaptchaAttemps;
+        public int Minutes;
+        public string? Night;
+        public string? State;
+        public SettingsState? SettingsState;
+        public long StateOwner;
+        public string? Gmt;
+        public bool IsReceive;
+        public ChatState ChatState;
+        public DateTime? StExpire;
+
+        public Chat() {
+            MaxWarns = 4;
+            Antispam = 4;
+            AsActive = false;
+            DetectUrl = false;
+            ReportStorage = 0;
+            CaptchaButton = CaptchaType.None;
+            NoBadWordsActive = false;
+            Punish = PunishType.Mute;
+            MaxCaptchaAttemps = 3;
+            Minutes = 5;
+            StateOwner = 0;
+            IsReceive = true;
+            ChatState = ChatState.State;
+        }
+    }
+}
