a73c4879adc3a845803b15d82c36003460395f4b
TheLance <98957643+LanceMinecraftFBA@users.noreply.github.com>
2023-06-14 21:53:14 +0300

    Create RandomCountProfile.cs

---
+++ src/Types/Games/RandomCountProfile.cs
@@ -0,0 +1,12 @@
+using LauraChatManager.Types.Games.Enum;
+
+namespace LauraChatManager.Types.Games {
+    public class RandomCountProfile {
+        public RcDifficulty LastDifficulty;
+        public int HighScoreEasy;
+        public int HighScoreMedium;
+        public int HighScoreHard;
+        public int HighScoreInsane;
+        public int HighScoreDemon;
+    }
+}
