48d7721ebe1bb911e7d685d4b296212b46d36938
TheLance <98957643+LanceMinecraftFBA@users.noreply.github.com>
2023-06-14 21:49:53 +0300

    Create FbaPosts.cs

---
+++ src/Types/FbaPosts.cs
@@ -0,0 +1,10 @@
+namespace LauraChatManager.Types {
+    public class FbaPosts {
+        public string Text;
+        public int PostID;
+        public string? Photo;
+        public string? Video;
+        public string? File;
+        public string? Audio;
+    }
+}
