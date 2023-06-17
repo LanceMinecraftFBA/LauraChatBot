cc12dc984530d38e6b935f135a00660bd5a75ba2
TheLance <98957643+LanceMinecraftFBA@users.noreply.github.com>
2023-06-17 16:49:42 +0300

    Create NsfwObject.cs

---
+++ src/Types/NsfwObject.cs
@@ -0,0 +1,9 @@
+namespace LauraChatManager.Types;
+
+public class NsfwObject { 
+    public double Porn;
+    public double Sexy;
+    public double Hentai;
+    public double Neutral;
+    public double Drawing;
+}
