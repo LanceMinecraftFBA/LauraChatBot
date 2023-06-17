46ff9c4abe755d9042a2aadfa9c7145917be3fc2
TheLance <98957643+LanceMinecraftFBA@users.noreply.github.com>
2023-06-17 16:50:28 +0300

    Create NsfwDetector.cs

---
+++ src/Methods/NsfwDetector.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using Newtonsoft.Json;
+using LauraChatManager.Types;
+using LauraChatManager.Configuration;
+
+namespace LauraChatManager.Methods;
+
+public class NsfwDetector {
+    public static readonly string header = "multipart/form-data";
+  
+    public static async Task<NsfwObject> GetNsfwScan(string path) {
+        try {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                using (MultipartFormDataContent formData = new MultipartFormDataContent())
+                {
+                    FileStream imageStream = File.OpenRead(path);
+                    formData.Add(new StreamContent(imageStream), "image", "image.jpg");
+
+                    HttpResponseMessage response = await httpClient.PostAsync(Config.LocalNswfDetector, formData);
+                    string responseText = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<NsfwObject>(responseText);
+                }
+            }
+        }
+        catch {
+            return new();
+        }
+    }
+}
