6f1fe112ff60578dfca8303c856ae87c26b23e66
TheLance <98957643+LanceMinecraftFBA@users.noreply.github.com>
2023-06-18 22:25:54 +0300

    Delete src/Handles/UpdateHandler directory

--- src/Handles/UpdateHandler/Callback/UserHandler.cs
+++
@@ -1,9 +0,0 @@
-using Telegram.Bot;
-
-namespace LauraChatManager.Handles.UpdateHandlers.Callback {
-    public class UserHandler {
-        public static async Task Invoke(ITelegramBotClient bot, Telegram.Bot.Types.CallbackQuery call) {
-
-        }
-    }
-}
--- src/Handles/UpdateHandler/Message/ChatHandler.cs
+++
@@ -1,9 +0,0 @@
-using Telegram.Bot;
-
-namespace LauraChatManager.Handles.UpdateHandlers.Message {
-    public class ChatHandler {
-        public static async Task Invoke(ITelegramBotClient bot, Telegram.Bot.Types.Message msg) {
-            
-        }
-    }
-}
