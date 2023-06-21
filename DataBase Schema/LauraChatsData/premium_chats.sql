c303b1383d74823e9fd433b39af2d7aab2bcf59e
TheLance <98957643+LanceMinecraftFBA@users.noreply.github.com>
2023-06-21 13:19:56 +0300

    Update premium_chats.sql

--- DataBase Schema/LauraChatsData/premium_chats.sql
+++ DataBase Schema/LauraChatsData/premium_chats.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `premium_chats` (
-  `id` int NOT NULL,
+  `id` int PRIMARY KEY AUTO_INCREMENT,
   `chat_id` varchar(255) NOT NULL,
   `expire` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
