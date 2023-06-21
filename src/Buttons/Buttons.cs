9031446077f3461e926ae270b9784a9ff86770ee
TheLance <98957643+LanceMinecraftFBA@users.noreply.github.com>
2023-06-21 14:22:47 +0300

    Create Buttons.cs

---
+++ src/Buttons/Buttons.cs
@@ -0,0 +1,17 @@
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace LauraChatManager.Buttons;
+
+public class Buttons {
+    public static ReplyKeyboardMarkup GetUserMenu() {
+        KeyboardButtonRequestChat chat = new();
+        var buttons = new ReplyKeyboardMarkup(new[] {
+                new KeyboardButton[] {
+                    new KeyboardButton("О боте🧸"),
+                    KeyboardButton.WithRequestChat("Выбрать чат⚙️", new() {BotIsMember = true, BotAdministratorRights = new() {CanDeleteMessages = true, CanRestrictMembers = true, CanManageChat = true}, UserAdministratorRights = new() {CanChangeInfo = true, CanDeleteMessages = true, CanInviteUsers = true, CanRestrictMembers = true, CanPinMessages = true, CanManageChat = true}, ChatIsChannel = false})
+                }
+            });
+        buttons.ResizeKeyboard = true;
+        return buttons;
+    }
+}
