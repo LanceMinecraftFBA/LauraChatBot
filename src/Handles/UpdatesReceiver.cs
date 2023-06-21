afc05502f0c1530effb1ba4b4ca27c5e1cfbc664
TheLance <98957643+LanceMinecraftFBA@users.noreply.github.com>
2023-06-21 13:01:34 +0300

    Update UpdatesReceiver.cs

--- src/Handles/UpdatesReceiver.cs
+++ src/Handles/UpdatesReceiver.cs
@@ -17,8 +17,9 @@ namespace LauraChatManager.Handles {
                     await LauraChatManager.Events.Message.InvokeMessage(bot, update.Message);
                     break;
                 case UpdateType.ChannelPost:
-                    TimeSpan time1 = DateTime.UtcNow - update.Message.Date ;
+                    TimeSpan time1 = DateTime.UtcNow - update.ChannelPost.Date ;
                     if(time1.TotalMinutes >= 5)
+                        break;
                     await LauraChatManager.Events.Message.InvokeMessage(bot, update.ChannelPost);
                     break;
                 case UpdateType.CallbackQuery:
