194bc9657a716d0382dec756986f2c2c6824e6b2
TheLance <98957643+LanceMinecraftFBA@users.noreply.github.com>
2023-06-21 13:16:41 +0300

    Create premium_chats.sql

---
+++ DataBase Schema/LauraChatsData/premium_chats.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `premium_chats` (
+  `id` int NOT NULL,
+  `chat_id` varchar(255) NOT NULL,
+  `expire` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
